afa05804bb9bd32f3e66356a88be9993dafc53d9
IlhamAgusk <ilhamagusk109@gmail.com>
2024-03-09 19:21:47 +0700

    INI COMMIT COBA

---
+++ app/src/main/java/com/example/coba/coba12.java
@@ -0,0 +1,15 @@
+package com.example.coba;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class coba12 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coba12);
+
+    }
+}
